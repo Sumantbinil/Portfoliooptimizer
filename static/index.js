4a25c7159b24b9956a723ecd3c14bfbe9dae189a
Sumantbinil <70511553+Sumantbinil@users.noreply.github.com>
2022-08-22 09:03:05 +0530

    Add files via upload

--- static/index.js
+++ static/index.js
@@ -1,47 +1,42 @@
-let globalChart,gmin,gmax=undefined;
-
+let globalChart = [];
 const submit = async (e) => {
-    if (globalChart != undefined) {
-      globalChart.destroy();
-      globalChart=undefined;
-    }
-    if (gmin != undefined) {
-      gmin.destroy();
-      gmin=undefined;
-    }
-    if (gmax != undefined) {
-      gmax.destroy();
-      gmax=undefined;
-    }
+  globalChart.forEach((g) => g.destroy());
+  globalChart = [];
+ 
+  e.preventDefault();
 
   const stock = document.getElementById("stock").value;
+  if(stock.split(" ").length<2){
+    document.getElementById("stock").classList.add("is-invalid");
+    document.getElementById("errorHelp").innerHTML = "Minimum two stock tickers required"
+    minRiskDiv.innerHTML = "";
+    maxReturnDiv.innerHTML="";
+    return
+  }
+  else{
+    document.getElementById("stock").classList.remove("is-invalid");
+    document.getElementById("errorHelp").innerHTML="";
+  }
+  document.getElementById("loading").style.display = "block";
+  document.getElementById("submit").disabled = true;
 
+  const response = await fetch(
+    "http://127.0.0.1:5000/api/portfolio?tickers=" + stock,
+    {
+      method: "GET",
+    }
+  );
+  
+  const json = await response.json();
   const minRiskDiv = document.getElementById("min-risk");
   minRiskDiv.innerHTML = '<p class="risktitle">MIN RISK </p>';
 
   const maxReturnDiv = document.getElementById("max-return");
   maxReturnDiv.innerHTML = '<p class="risktitle">MAX RETURN</p>';
-  
-  if(stock.split(" ").length<2){
-    document.getElementById("error").innerHTML="Minimum two input required";
-    minRiskDiv.innerHTML = "";
-    maxReturnDiv.innerHTML="";
-  }
-  else{
-    document.getElementById("error").innerHTML="";
-  
-  document.getElementById("loading").style.display = "block";
-  document.getElementById("submit").disabled = true;
-
-
-
-  e.preventDefault();
-  const response = await fetch('http://127.0.0.1:5000/api/portfolio?tickers=' + stock, {
-    method: 'GET',
-  });
-  const json = await response.json();
-  console.log(json);
   const minRisk = JSON.parse(json.minRisk);
+
+
+
   const maxReturn = JSON.parse(json.maxReturn);
   const df = JSON.parse(json.df);
   const minRiskTable = getTable(minRisk);
@@ -51,33 +46,29 @@ const submit = async (e) => {
   document.getElementById("submit").disabled = false;
   document.getElementById("loading").style.display = "none";
 
-  let y = []
-  let x = []
+  let y = [];
+  let x = [];
   for (let [key, value] of Object.entries(minRisk)) {
-    console.log(value);
     let k = Object.keys(value)[0];
     let v = value[k];
-    x.push(key)
-    y.push(v)
+    x.push(key);
+    y.push(v);
   }
   y.splice(0, 3);
   x.splice(0, 3);
 
-  let ym = []
-  let xm = []
+  let ym = [];
+  let xm = [];
   for (let [key, value] of Object.entries(maxReturn)) {
-    console.log(value);
     let k = Object.keys(value)[0];
     let v = value[k];
-    xm.push(key)
-    ym.push(v)
+    xm.push(key);
+    ym.push(v);
   }
   ym.splice(0, 3);
   xm.splice(0, 3);
-  console.log(xm);
-  console.log(ym);
 
-  let colors = ["red", "green", "blue", "yellow", "purple"];
+  let colors = ["red", "green", "blue", "yellow", "purple","black"];
   let xValues = [];
   df.index.forEach((e) => {
     let date = new Date(e);
@@ -95,66 +86,101 @@ const submit = async (e) => {
     };
     datasets.push(data);
   });
-  console.log(datasets);
+  console.log({datasets,xValues});
   df.data.forEach((element) => {
     element.forEach((e, i) => {
       datasets[i].data.push(e);
     });
   });
 
-  globalChart = new Chart("myChart", {
-    type: "line",
-    data: {
-      labels: xValues,
-      datasets: datasets,
-    },
-    options: {
-      legend: { display: false },
-    },
-  });
-  gmin = new Chart("mypie", {
-    type: "pie",
-    data: {
-      labels: x,
-      datasets: [{
-        label: 'Min Risk Piechart',
-        data: y,
-        backgroundColor: colors,
 
-      }]
-    },
-    options: {
-      legend: { display: false }
-    }
-  });
-  gmax = new Chart("mypiem", {
-    type: "pie",
-    data: {
-      labels: xm,
-      datasets: [{
-        label: 'Max Return Piechart',
-        data: ym,
-        backgroundColor: colors,
-
-      }]
-    },
-    options: {
-      legend: { display: false }
-    }
-  });
-
+  globalChart.push(
+    new Chart("mypie", {
+      type: "pie",
+      data: {
+        labels: x,
+        datasets: [
+          {
+            label: "My First Dataset",
+            data: y,
+            backgroundColor: colors,
+          },
+        ],
+      },
+      options: {
+        title: {
+          display: true,
+          text: 'MINIMUM RISK PORTFOLIO'
+        },
+        legend: {
+          position: 'top',
+        },
+      },
+    })
+  );
+  globalChart.push(
+    new Chart("mypiem", {
+      type: "pie",
+      data: {
+        labels: xm,
+        datasets: [
+          {
+            label: "My First Dataset",
+            data: ym,
+            backgroundColor: colors,
+          },
+        ],
+      },
+      options: {
+        title: {
+          display: true,
+          text: 'MAXIMUM RETURN PORTFOLIO'
+        },
+        legend: {
+          position: 'top',
+        },
+      },
+    })
+  );
+  document.getElementById("leftchartname").innerHTML = 'MINIMUM RISK PORTFOLIO'
+  document.getElementById("rightchartname").innerHTML = 'MAXIMUM RETURN PORTFOLIO'
+  const k=document.getElementById("allgraph")
+  k.innerHTML = undefined
+  datasets.forEach((datast)=>{
+    newGraph({xValues,datasets:[datast]},datast.label,k,globalChart)
+  })
+  newGraph({xValues,datasets},'myChart',k,globalChart)
 };
-  }
-  
 document.getElementById("submit").addEventListener("click", submit);
 
-const getTable = (data) => {
+const getTable = (data,money=1) => {
   let table = document.createElement("table");
-  table.className = "risktable";
+  table.className = "table table-bordered";
   for (let [key, value] of Object.entries(data)) {
     let k = Object.keys(value)[0];
-    let v = value[k];
-    table.innerHTML += `<tr> <td>${key}</td> <td>${v}</td></tr>`;
+    let v = parseFloat(value[k]).toFixed(6)*money;
+
+    table.innerHTML += `<tr> <td class="key_name">${key}</td> <td>${v}</td></tr>`;
   }
   return table;
 };
+
+const newGraph = (data,name,el,globalcharts) => {
+  const canv = document.createElement('canvas')
+  canv.style ="max-height: 500px; max-width: 1000px;"
+  canv.id=name
+  el.appendChild(canv)
+
+  globalcharts.push(
+    new Chart(name, {
+      type: "line",
+      data: {
+        labels: data.xValues,
+        datasets: data.datasets,
+      },
+      options: {
+        legend: { display: false },
+      },
+    })
+  )
+}
