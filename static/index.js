0318b753eae489382499b97afed7fba42e289cb9
Sumantbinil <70511553+Sumantbinil@users.noreply.github.com>
2022-08-22 10:23:04 +0530

    Updated index.js with openshift link

--- static/index.js
+++ static/index.js
@@ -21,7 +21,7 @@ const submit = async (e) => {
   document.getElementById("submit").disabled = true;
 
   const response = await fetch(
-    "http://127.0.0.1:5000/api/portfolio?tickers=" + stock,
+    "http://portfolioapi-portfolioapi.linuxapacgtc40.conygre.com/api/portfolio?tickers=" + stock,
     {
       method: "GET",
     }
